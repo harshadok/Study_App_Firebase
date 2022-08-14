e447091baf4a123db82d1b4fcb4ed3b30287a6a4
harshadok <harshadodakkal@gmail.com>
2022-08-14 19:39:51 +0530

    data added to model

--- firebase_studyapp/lib/controllers/qustion_peoper/data_uploder.dart
+++ firebase_studyapp/lib/controllers/qustion_peoper/data_uploder.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:firebase_studyapp/models/qustion_paper_model.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
@@ -21,9 +22,14 @@ class DataUploder extends GetxController {
         .where((element) =>
             element.startsWith("assets/DB/papers") && element.contains(".json"))
         .toList();
+
+    List<QustionPaperModel> qustionPapers = [];
     for (var paper in paperinAssets) {
       String stringPaperContent = await rootBundle.loadString(paper);
-      print(stringPaperContent);
+      // print(stringPaperContent);
+
+      qustionPapers
+          .add(QustionPaperModel.fromJson(jsonDecode(stringPaperContent)));
     }
   }
 }
